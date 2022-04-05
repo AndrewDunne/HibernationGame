52c974eaf2dab281f81429859f8c30148cef920c
AndrewDunne <57766413+AndrewDunne@users.noreply.github.com>
2022-04-04 17:47:55 -0700

    v1 done!

--- Hibernation Game/Assets/Scripts/pillowScript.cs
+++ Hibernation Game/Assets/Scripts/pillowScript.cs
@@ -5,17 +5,19 @@ using UnityEngine;
 public class pillowScript : MonoBehaviour
 {
     Color pillowpacity = new Color(1f, 1f, 1f, 1f);
+    float spawntime;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawntime = Time.timeSinceLevelLoad;
     }
 
     // Update is called once per frame
     void Update()
     {
+        float opac = 1 - ((Time.timeSinceLevelLoad - spawntime) / 1.4f);
         GetComponent<SpriteRenderer>().color = pillowpacity;
-        pillowpacity.a -= .002f;
+        pillowpacity.a = opac;
         if(pillowpacity.a <= 0)
         {
             DestroyObject(gameObject);
